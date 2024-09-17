f5f956914f432a15d70959d497e301e99bd4c883
trungtn <tangoctrung12123@gmail.com>
2024-09-17 20:16:14 +0700

    build ui input chat

--- src/components/PageMess/ChatDetail/InputChat/index.tsx
+++ src/components/PageMess/ChatDetail/InputChat/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import style from "./inputChat.module.css"
+import { FaRegFileImage } from "react-icons/fa6";
+import { LuSticker } from "react-icons/lu";
+import { MdInsertEmoticon } from "react-icons/md";
 
 function InputChat() {
     return (
-        <div className={style.containerInputChat}>InputChat</div>
+        <div className={style.containerInputChat}>
+            <div className={`${style.containerIcon}`}>
+                <FaRegFileImage />
+            </div>
+            <form className={`${style.formInput}`}>
+                <input type='text' placeholder='Nhập gì đó để gửi...' className={style.input} />
+                <div className={style.iconInput}>
+                    <MdInsertEmoticon className={style.iconEmoji} />
+                </div>
+            </form>
+            <div className={`${style.containerIcon}`}>
+                <LuSticker />
+            </div>
+        </div>
     )
 }
 
